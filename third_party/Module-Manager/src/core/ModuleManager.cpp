b3d3412e2229b07cea62fe2da9dc012158ef29e4
bradosia <32529373+bradosia@users.noreply.github.com>
2021-07-31 09:43:15 -0700

    fix build

---
+++ third_party/Module-Manager/src/core/ModuleManager.cpp
@@ -0,0 +1,16 @@
+/*
+ * @name Module Manager
+ * @author Branden Lee
+ * @version 1.01
+ * @license GNU LGPL v3
+ * @brief Manages shared library plugins
+ */
+
+// Local Project
+#include "ModuleManager.hpp"
+
+namespace bradosia {
+
+  
+
+} // namespace bradosia
